cc06d270013f8f7a7d73c280303225db8c0ab82a
LeonardiLaura <73897108+LeonardiLaura@users.noreply.github.com>
2022-06-20 19:33:30 +0200

    cleaning

--- arduino+esp/bozza2/Config.h
+++
@@ -1,4 +0,0 @@
-/** configurazione pin */
-#define TEMP 7
-#define LED 3
-#define PHOTORES 6
--- arduino+esp/progetto_03_bozza/Config.h
+++
@@ -1,8 +0,0 @@
-/** configurazione pin */
-#define L1_FADE 5
-#define L2_FADE 6
-#define L1_SWITCH 7
-#define L2_SWITCH 8
-#define SERVO_MOTOR_PIN 9
-#define BLUE_RXD 3
-#define BLUE_TXD 2
